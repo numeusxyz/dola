4297b4f5d555c4c071960fd2c1d2003835500fb1
Yordan Miladinov <jordanmiladinov@gmail.com>
2021-08-17 13:36:58 +0300

    remove init() from dola.go, I'll use the enhanced crypto/rand in the future

--- dola.go
+++ dola.go
@@ -2,13 +2,3 @@
 // integrations (via github.com/thrasher-corp/gocryptotrader),
 // event-driven strategies, utilities and more.
 package dola
-
-import (
-	"math/rand"
-	"time"
-)
-
-// nolint:gochecknoinits
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
