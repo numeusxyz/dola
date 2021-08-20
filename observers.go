31b368b454930a2066d88c43b3ea955e77ed875c
Yordan Miladinov <jordanmiladinov@gmail.com>
2021-08-20 22:55:16 +0300

    OnFilledObserver: enrich interface + add Slots (convenient for composition)

--- observers.go
+++ observers.go
@@ -6,5 +6,20 @@ import (
 )
 
 type OnFilledObserver interface {
-	OnFilled(e exchange.IBotExchange, x order.Detail)
+	OnFilled(k *Keep, e exchange.IBotExchange, x order.Detail)
+}
+
+// +-------+
+// | Slots |
+// +-------+
+
+type Slots struct {
+	OnFilledSlot func(k *Keep, e exchange.IBotExchange, x order.Detail)
+}
+
+// OnFilled implements OnFilledObserver.
+func (s Slots) OnFilled(k *Keep, e exchange.IBotExchange, x order.Detail) {
+	if s.OnFilledSlot != nil {
+		s.OnFilledSlot(k, e, x)
+	}
 }
