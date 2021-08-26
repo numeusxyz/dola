dd007ed3092b44d0920d886c86f10b4db8c075fd
Yordan Miladinov <yordan.miladinov@flolive.net>
2021-08-26 12:14:44 +0300

    logging: implement LogState and AwakenLogger

--- logging.go
+++ logging.go
@@ -1,7 +1,6 @@
 package dola
 
 import (
-	"sync"
 	"sync/atomic"
 	"time"
 
@@ -9,60 +8,97 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// +------------------+
-// | Stateful logging |
-// +------------------+
+// +----------+
+// | LogState |
+// +----------+
 
 const (
 	// In dormant mode the StatefulLogger outputs trace logs as info every once in a
 	// while.
 	Dormant = iota
-	// In operational mode everything is outputted as-is.
-	Operational = iota
+	// In awaken mode everything is outputted as-is.
+	Awaken = iota
 )
 
-type StatefulLogger struct {
+type LogState struct {
 	state int32
-
-	traceLast  time.Time
-	traceEvery time.Duration
-
+	// Time of last awakening.
 	awakenAt time.Time
-
-	m sync.Mutex
+	// For how long state should be kept Awaken.
+	duration time.Duration
 }
 
-func NewTimedLogger() StatefulLogger {
+func NewLogState(duration time.Duration) LogState {
+	return LogState{
+		state:    Dormant,
+		awakenAt: time.Time{},
+		duration: duration,
+	}
+}
+
+func (s *LogState) WakeUp() {
+	if atomic.CompareAndSwapInt32(&s.state, Dormant, Awaken) {
+		s.awakenAt = time.Now()
+	}
+}
+
+func (s *LogState) Awaken() bool {
+	if time.Since(s.awakenAt) < s.duration {
+		return true
+	}
+
+	// Go back into dormant state, if not already.
+	atomic.CompareAndSwapInt32(&s.state, Awaken, Dormant)
+
+	return false
+}
+
+// +----------------+
+// | StatefulLogger |
+// +----------------+
+
+type StatefulLogger struct {
+	state LogState
+
+	traceEvery time.Duration
+	traceLast  time.Time
+}
+
+func NewStatefulLogger(d time.Duration) StatefulLogger {
+	// We use the same duration for both the time awaken and how
+	// often trace logs should be allowed.
 	return StatefulLogger{
-		state:      Dormant,
-		traceEvery: 0,
+		state:      NewLogState(d),
+		traceEvery: d,
 		traceLast:  time.Time{},
 	}
 }
 
-// WakeUp gets the StatefulLogger out of the dormant state for a predefined amount of
-// time.
+// WakeUp gets the StatefulLogger out of its dormant state for a an
+// amount of time.
 func (t *StatefulLogger) WakeUp() {
-	atomic.CompareAndSwapInt32(&t.state, Dormant, Operational)
+	t.state.WakeUp()
 }
 
 func (t *StatefulLogger) Trace() *zerolog.Event {
-	switch t.state {
-	case Dormant:
-		return t.dormantTrace()
-	case Operational:
-		return log.Trace()
+	if t.state.Awaken() {
+		return log.Info()
 	}
-	panic("illegal state")
+
+	return t.dormantTrace()
 }
 
+// dormantTrace is left unlocked on purpose.  If there is a race
+// condition and more than one thread set `traceLast` to Now(), we
+// don't care, it's still Now().
 func (t *StatefulLogger) dormantTrace() *zerolog.Event {
-	if time.Since(t.traceLast) < t.traceEvery {
+	if time.Since(t.traceLast) > t.traceEvery {
 		t.traceLast = time.Now()
-		return log.Trace()
+
+		return log.Info()
 	}
 
-	return log.Trace().Discard()
+	return log.Trace()
 }
 
 // +-------------------+
---
+++ logging_test.go
@@ -0,0 +1,40 @@
+package dola_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/numus-digital/dola"
+)
+
+func TestLogState(t *testing.T) {
+	t.Parallel()
+
+	state := dola.NewLogState(100 * time.Millisecond)
+
+	// Make sure repeated calls to Awaken() report the same state.
+	for i := 0; i < 3; i++ {
+		if state.Awaken() {
+			t.Error("have awaken, want dormant")
+		}
+	}
+
+	// Put into awaken state.
+	state.WakeUp()
+
+	// Make sure for the predefined duration state stays awaken.
+	for i := 0; i < 10; i++ {
+		if !state.Awaken() {
+			t.Error("have dormant, want awaken")
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	// After time expires, make sure state is dormant again.
+	for i := 0; i < 3; i++ {
+		if state.Awaken() {
+			t.Error("have awaken, want dormant")
+		}
+	}
+}
