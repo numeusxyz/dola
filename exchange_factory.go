4c5889c89232bb6fa54774033035f2f6df3f7f69
Yordan Miladinov <jordanMiladinov@gmail.com>
2021-09-03 11:32:31 +0300

    move out the keep version of ExchangeCreatorFunc as it has nothing to do with the exchange factory

--- exchange_factory.go
+++ exchange_factory.go
@@ -8,19 +8,16 @@ import (
 
 var ErrCreatorNotRegistered = errors.New("exchange creator not registered")
 
-type ExchangeCreatorFuncK func(k *Keep) (exchange.IBotExchange, error)
-
-// ExchangeCreatorFunc type is the func type being stored in the factory map
-// GCT doesn't allow us to carry context.
-type ExchangeCreatorFunc func() (exchange.IBotExchange, error)
-
-type ExchangeFactory map[string]ExchangeCreatorFunc
+type (
+	ExchangeCreatorFunc func() (exchange.IBotExchange, error)
+	ExchangeFactory     map[string]ExchangeCreatorFunc
+)
 
 func (e ExchangeFactory) Register(name string, fn ExchangeCreatorFunc) {
 	e[name] = fn
 }
 
-// NewExchangeByName is satisfying GCT's interface CustomExchangeBuilder.
+// NewExchangeByName implements gocryptotrader/engine.CustomExchangeBuilder.
 func (e ExchangeFactory) NewExchangeByName(name string) (exchange.IBotExchange, error) {
 	fn, ok := e[name]
 	if !ok {
