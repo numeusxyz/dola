63d47279878dd179a5391995e8da1c10cd848895
Yordan Miladinov <jordanmiladinov@gmail.com>
2021-09-07 10:17:18 +0300

    initially commit the toolbelt -- helper functions

---
+++ toolbelt.go
@@ -0,0 +1,30 @@
+package dola
+
+import (
+	"errors"
+
+	"github.com/thrasher-corp/gocryptotrader/exchanges/account"
+)
+
+var (
+	ErrNeedBalancesStrategy = errors.New("Keep should be configured with balances support")
+	ErrCast                 = errors.New("casting failed")
+)
+
+func CurrencyBalance(k *Keep, exchangeName, currencyCode string, accountIndex int) (account.Balance, error) {
+	st, err := k.Root.Get("balances")
+	if errors.Is(err, ErrStrategyNotFound) {
+		var empty account.Balance
+
+		return empty, ErrNeedBalancesStrategy
+	}
+
+	balances, ok := st.(*BalancesStrategy)
+	if !ok {
+		var empty account.Balance
+
+		return empty, ErrCast
+	}
+
+	return balances.Currency(exchangeName, currencyCode, accountIndex)
+}
